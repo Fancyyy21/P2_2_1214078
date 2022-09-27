ef44d6ec72fe689f8606626a1eb2410c2e268c50
LANA <imanulhaq5@gmail.com>
2022-09-27 10:03:08 +0700

    Add project files.

---
+++ p2_2_1214078/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2_2_1214078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Nama = Maulana Imanulhaq Nurdiana
+              NPM = 1214078*/
+
+            Console.WriteLine("===============================");
+
+            Console.WriteLine("Silakan Masukkan Jumlah Gaji Disini !");
+
+            Console.WriteLine("===============================");
+
+            Console.Write("Nominal Gaji Kamu = ");
+            float gaji = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("===============================");
+
+            Console.Write("1. Tunjangan (20% dari gaji pokok)             = ");
+            Console.WriteLine(gaji * 20 / 100);
+
+            Console.Write("2. Bonus (15% dari gaji pokok)                 = ");
+            Console.WriteLine(gaji * 15 / 100);
+
+            Console.Write("3. PPH (3,5% dari gaji pokok)                  = ");
+            Console.WriteLine(gaji * 35 / 1000);
+
+            Console.Write("4. Total Gaji (Gaji Pokok + Tunjangan + Bonus) = ");
+            Console.WriteLine((gaji) + (gaji * 20 / 100) + (gaji * 15 / 100));
+
+            Console.Write("5. Gaji Bersih (Total Gaji – PPH)              = ");
+            Console.WriteLine((gaji) - (gaji * 35 / 1000));
+
+            Console.WriteLine("===============================");
+            Console.WriteLine("===============================");
+
+
+        }
+    }
+}
