39f7aa32f6580efe6ce0a287b44ded1d9c8131cc
LANA <imanulhaq5@gmail.com>
2022-09-28 11:37:27 +0700

    try catch

--- p2_2_1214078/Program.cs
+++ p2_2_1214078/Program.cs
@@ -20,28 +20,37 @@ namespace p2_2_1214078
 
             Console.WriteLine("===============================");
 
-            Console.Write("Nominal Gaji Kamu = ");
-            float gaji = float.Parse(Console.ReadLine());
+            
+            try 
+            {
+                Console.Write("Nominal Gaji Kamu = ");
+                float gaji = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("===============================");
+                Console.WriteLine("===============================");
 
-            Console.Write("1. Tunjangan (20% dari gaji pokok)             = ");
-            Console.WriteLine(gaji * 20 / 100);
+                Console.Write("1. Tunjangan (20% dari gaji pokok)             = ");
+                Console.WriteLine(gaji * 20 / 100);
 
-            Console.Write("2. Bonus (15% dari gaji pokok)                 = ");
-            Console.WriteLine(gaji * 15 / 100);
+                Console.Write("2. Bonus (15% dari gaji pokok)                 = ");
+                Console.WriteLine(gaji * 15 / 100);
 
-            Console.Write("3. PPH (3,5% dari gaji pokok)                  = ");
-            Console.WriteLine(gaji * 35 / 1000);
+                Console.Write("3. PPH (3,5% dari gaji pokok)                  = ");
+                Console.WriteLine(gaji * 35 / 1000);
 
-            Console.Write("4. Total Gaji (Gaji Pokok + Tunjangan + Bonus) = ");
-            Console.WriteLine((gaji) + (gaji * 20 / 100) + (gaji * 15 / 100));
+                Console.Write("4. Total Gaji (Gaji Pokok + Tunjangan + Bonus) = ");
+                Console.WriteLine((gaji) + (gaji * 20 / 100) + (gaji * 15 / 100));
 
-            Console.Write("5. Gaji Bersih (Total Gaji – PPH)              = ");
-            Console.WriteLine((gaji) - (gaji * 35 / 1000));
+                Console.Write("5. Gaji Bersih (Total Gaji – PPH)              = ");
+                Console.WriteLine((gaji) - (gaji * 35 / 1000));
 
-            Console.WriteLine("===============================");
-            Console.WriteLine("===============================");
+                Console.WriteLine("===============================");
+                Console.WriteLine("===============================");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Terjadi kesalahan \n" + e.Message); ;
+                Console.ReadLine();
+            }
 
 
         }
